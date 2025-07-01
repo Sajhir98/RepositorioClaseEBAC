900a22441a124469b41f08a38ba1497145ae9a90
Sajhir98 <allanuc1428@gmail.com>
2025-07-01 15:50:20 -0500

    Hola Mundo
    Se añadio el scrip en unity de Hola mundo y se hicieron las pruebas requeridas

---
+++ Ensayo EBAC/Assets/Scripts/HolaMundo2.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class HolaMundo2:MonoBehaviour
+{
+    void Start()
+    {
+        print("algo paso");
+        Debug.Log("Algo Paso");
+        Debug.LogWarning("Algo Salio Medianamente Mal");
+        Debug.LogError("Algo Salio Muy Mal");
+    }
+}
