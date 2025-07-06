c5135b4a068f480f175a02651960a791e39fd2a3
Sajhir98 <allanuc1428@gmail.com>
2025-07-05 19:18:27 -0500

    Ensayo de practica
    Se hace el ensayo de impresiónes en consola de unity en una nueva branch, se procede a hacer el merge to main

--- Ensayo EBAC/Assets/HolaMundo2.cs
+++ Ensayo EBAC/Assets/HolaMundo2.cs
@@ -16,7 +16,28 @@ public class HolaMundo2:MonoBehaviour
     // update is called once per frame 
     private void Update()
     {
-        x = x + 1;
-       Debug.Log(x); 
+        //x = x + 1;
+        //Debug.Log(x); 
+
+        Debug.Log("Hola desde Update");
+    }
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Hola desde Fixed Update cada 50 frames");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("Hola desde Late update");
+    }
+
+    private void OnEnable()
+    {
+        Debug.LogWarning("El objeto ha sido habilitado)");
+    }
+
+    private void OnDisable()
+    {
+        Debug.LogWarning("El Objeto ha sido inhabilitado)");
     }
 }
